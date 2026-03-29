fcc80a5cc2490b7e81cdf0ce244ba31967fc7de0
IsvaraP <ipalhad@gmail.com>
2026-03-29 16:33:44 +0200

    Update GitHubTestScript.java
    Edit to test hooks

--- GitHubTestScript.java
+++ GitHubTestScript.java
@@ -1,7 +1,7 @@
 public class GitHubTestScript {
     public static void main(String[] args) {
         System.out.println("Hey this is my script for GIT man              " +
-                "sdcfwsfswf");
+                "Edit to test new change and GIT hooks");
 
     }
 }
