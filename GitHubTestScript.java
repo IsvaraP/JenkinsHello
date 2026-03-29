4f9ec03d5e0fa51e20c1eb963bbcf0e8ec0803e9
unknown <ipalhad@gmail.com>
2026-03-29 16:20:22 +0200

    Adding files

---
+++ GitHubTestScript.java
@@ -0,0 +1,7 @@
+public class GitHubTestScript {
+    public static void main(String[] args) {
+        System.out.println("Hey this is my script for GIT man              " +
+                "sdcfwsfswf");
+
+    }
+}
