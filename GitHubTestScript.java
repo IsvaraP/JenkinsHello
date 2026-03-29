c8aeb6cfdbb85e6ba1d4712cb5ba0c8584c07a5a
IsvaraP <ipalhad@gmail.com>
2026-03-29 17:33:49 +0200

    Update GitHubTestScript.java

--- GitHubTestScript.java
+++ GitHubTestScript.java
@@ -1,7 +1,7 @@
 public class GitHubTestScript {
     public static void main(String[] args) {
         System.out.println("Hey this is my script for GIT man              " +
-                "Edit to test new change and GIT hooks");
+                "Edit to test new change and GIT hooks 2nd try");
 
     }
 }
